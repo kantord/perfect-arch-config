8913bbf04dbf33f5d7c4292d54b70952c5259b8f
Dániel Kántor <git@daniel-kantor.com>
2019-04-24 10:32:51 +0200

    i3-msg: only show rofi if there's more than one option

--- i3_msg_rofi.sh
+++ i3_msg_rofi.sh
@@ -3,7 +3,14 @@
 current_display=`i3-msg -t get_workspaces | emuto 'filter ($ => $.focused) | $[0].output | [$]' -o=raw`
 active_displays=`~/repos/perfect-arch-config/list_active_displays.sh`
 available_displays=`echo $active_displays | sed "s/ /\n/g" | grep -v $current_display`
-message=`echo $available_displays | sed "s/ /\n/g" | sed "s/^/move workspace to output /" | rofi -dmenu`
+number_of_options=`echo $available_displays | sed "s/ /\n/g" | sed "s/^/move workspace to output /" | wc -l`
+
+if (( number_of_options <= 1 ))
+then
+	message=`echo $available_displays | sed "s/ /\n/g" | sed "s/^/move workspace to output /"`
+else
+	message=`echo $available_displays | sed "s/ /\n/g" | sed "s/^/move workspace to output /" | rofi -dmenu`
+fi
 
 if [ -z "$message" ]
 then
