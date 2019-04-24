a8f6530b3d6806771b985e8ebabffd3c13622a6c
Dániel Kántor <git@daniel-kantor.com>
2019-04-24 10:01:31 +0200

    better way to identify connected displays

--- restart_gui.sh
+++ restart_gui.sh
@@ -7,6 +7,6 @@ i3-msg reload
 exec compton --config ~/repos/perfect-arch-config/dotfiles/compton/.config/compton/compton.conf &
 exec twmnd &
 
-for m in $(xrandr | grep connected | grep -v disconn | sed 's/ .*//'); do
+for m in $(xrandr | grep connected | grep -v disconnected | grep "[0-9]\+x" | sed "s/ .*//"); do
     MONITOR=$m polybar --reload example &
 done
