028c4f9acad311e6acf8c4fc89804dbb10eb5f2d
Dániel Kántor <git@daniel-kantor.com>
2019-05-13 12:54:29 +0200

    add unclutter

--- restart_gui.sh
+++ restart_gui.sh
@@ -3,9 +3,11 @@
 killall compton
 killall polybar
 killall twmnd
+killall unclutter
 i3-msg reload
 exec compton --config ~/repos/perfect-arch-config/dotfiles/compton/.config/compton/compton.conf &
 exec twmnd &
+exec unclutter &
 
 for m in $(~/repos/perfect-arch-config/list_active_displays.sh); do
     MONITOR=$m polybar --reload example &
