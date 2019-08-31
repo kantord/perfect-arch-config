09bfda537c051304684ac395bc2688423b339e67
Dániel Kántor <git@daniel-kantor.com>
2019-08-31 16:04:48 +0200

    Automatically deduplicate package list

--- addPackage.sh
+++ addPackage.sh
@@ -2,3 +2,6 @@
 
 echo "Adding package $1"
 yay -S $1 && echo "$1" >> ~/repos/perfect-arch-config/packages.txt
+cat ~/repos/perfect-arch-config/packages.txt | sort | uniq > /tmp/tmp_pkg_list.txt
+cat /tmp/tmp_pkg_list.txt > ~/repos/perfect-arch-config/packages.txt
+
