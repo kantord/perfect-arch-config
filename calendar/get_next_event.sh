9f7c63dbbc08d1135c86f4261045deff49cdee2e
Dániel Kántor <git@daniel-kantor.com>
2019-09-01 16:34:54 +0200

    also show next event on polybar

--- calendar/get_next_event.sh
+++ calendar/get_next_event.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-next_event_line=`khal list --notstarted -f "{start-date};={start-time};{title}" | grep = | grep -v ";=;" | head -n1 | sed 's ;= ; ' | sed 's/;/ /'`
+next_event_line=`khal list --notstarted now -f "{start-date};={start-time};{title}" | grep = | grep -v ";=;" | head -n1 | sed 's ;= ; ' | sed 's/;/ /'`
 next_event_datetime=`echo $next_event_line | cut -f1 -d";"`
-next_event_title=`echo $next_event_line | cut -f2 -d";" | cut -c -30`
+next_event_title=`echo $next_event_line | sed 's/\[regular\]$//' | cut -f2 -d";" | cut -c -30`
 next_event_time=`date '+%s' -d "$next_event_datetime"`
 now_time=`date '+%s' -d "now"`
 time_difference=$(($next_event_time - $now_time))
@@ -11,4 +11,7 @@ time_difference_hours=$(($time_difference_minutes / 60))
 time_difference_minutes_adjusted=$(($time_difference_minutes - ($time_difference_hours * 60) ))
 time_difference_formatted=`echo "$time_difference_hours"h "$time_difference_minutes_adjusted"m | sed 's/0h //'`
 
-echo "Next event:" $next_event_title - $time_difference_formatted"  "
+now_line=`khal at -f "{start-date};={start-time};{title}" | grep = | grep -v ";=;" | head -n1 | sed 's ;= ; ' | sed 's/;/ /'`
+now_title=`echo $now_line | cut -f2 -d";" | cut -c -30 | sed 's/$/  /' | sed 's/^/Now:/' | sed 's/Now: *//'`
+
+echo "$now_title""In $time_difference_formatted:" $next_event_title
