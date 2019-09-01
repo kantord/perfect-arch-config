5f2a1f5fb8e8e006ee68c32935b795e6fe6b26e7
Dániel Kántor <git@daniel-kantor.com>
2019-09-01 22:00:47 +0200

    increase notification time

--- notifications/statnot_conf.py
+++ notifications/statnot_conf.py
@@ -1,8 +1,8 @@
 # Default time a notification is show, unless specified in notification
-DEFAULT_NOTIFY_TIMEOUT = 4500 # milliseconds
+DEFAULT_NOTIFY_TIMEOUT = 6000 # milliseconds
 
 # Maximum time a notification is allowed to show
-MAX_NOTIFY_TIMEOUT = 6000 # milliseconds
+MAX_NOTIFY_TIMEOUT = 9000 # milliseconds
 
 # Maximum number of characters in a notification. 
 NOTIFICATION_MAX_LENGTH = 100 # number of characters
