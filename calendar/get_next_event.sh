461afa0de1021a186cb5f1ea52a2e03104caafce
Dániel Kántor <git@daniel-kantor.com>
2019-09-04 17:04:24 +0200

    hide no meetings day from calendar

--- calendar/get_next_event.sh
+++ calendar/get_next_event.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-next_event_line=`khal list --notstarted now -f "{start-date};={start-time};{title}" | grep = | grep -v ";=;" | head -n1 | sed 's ;= ; ' | sed 's/;/ /'`
+next_event_line=`khal list --notstarted now -f "{start-date};={start-time};{title}"  | grep -v "No meetings day /blocker/" | grep = | grep -v ";=;" | head -n1 | sed 's ;= ; ' | sed 's/;/ /'`
 next_event_datetime=`echo $next_event_line | cut -f1 -d";"`
 next_event_title=`echo $next_event_line | sed 's/\[regular\]$//' | cut -f2 -d";" | cut -c -30`
 next_event_time=`date '+%s' -d "$next_event_datetime"`
@@ -11,7 +11,7 @@ time_difference_hours=$(($time_difference_minutes / 60))
 time_difference_minutes_adjusted=$(($time_difference_minutes - ($time_difference_hours * 60) ))
 time_difference_formatted=`echo "$time_difference_hours"h "$time_difference_minutes_adjusted"m | sed 's/0h //'`
 
-now_line=`khal at -f "{start-date};={start-time};{title}" | grep = | grep -v ";=;" | head -n1 | sed 's ;= ; ' | sed 's/;/ /' | sed 's/\[regular\]$//'`
+now_line=`khal at -f "{start-date};={start-time};{title}" | grep -v "No meetings day /blocker/" | grep = | grep -v ";=;" | head -n1 | sed 's ;= ; ' | sed 's/;/ /' | sed 's/\[regular\]$//'`
 now_title=`echo $now_line | cut -f2 -d";" | cut -c -30 | sed 's/$/  /' | sed 's/^/Now:/' | sed 's/Now: *//'`
 
 next_summary=`echo "In $time_difference_formatted:" $next_event_title | sed 's/.*[0-9][0-9]\+h.*//'`
