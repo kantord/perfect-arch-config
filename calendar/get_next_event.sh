7e91c738d630d5e82bb1fa9ccb9711292055e1a0
Dániel Kántor <git@daniel-kantor.com>
2019-09-05 14:05:59 +0200

    fix bugs in calendar

--- calendar/get_next_event.sh
+++ calendar/get_next_event.sh
@@ -9,10 +9,10 @@ time_difference=$(($next_event_time - $now_time))
 time_difference_minutes=$(($time_difference / 60))
 time_difference_hours=$(($time_difference_minutes / 60))
 time_difference_minutes_adjusted=$(($time_difference_minutes - ($time_difference_hours * 60) ))
-time_difference_formatted=`echo "$time_difference_hours"h "$time_difference_minutes_adjusted"m | sed 's/0h //'`
+time_difference_formatted=`echo "$time_difference_hours"h "$time_difference_minutes_adjusted"m | sed 's/^0h //'`
 
 now_line=`khal at -f "{start-date};={start-time};{title}" | grep -v "No meetings day /blocker/" | grep = | grep -v ";=;" | head -n1 | sed 's ;= ; ' | sed 's/;/ /' | sed 's/\[regular\]$//'`
-now_title=`echo $now_line | cut -f2 -d";" | cut -c -30 | sed 's/$/  /' | sed 's/^/Now:/' | sed 's/Now: *//'`
+now_title=`echo $now_line | cut -f2 -d";" | cut -c -30 | sed 's/$/  /' | sed 's/^/Now:/' | sed 's/Now: *$//'`
 
 next_summary=`echo "In $time_difference_formatted:" $next_event_title | sed 's/.*[0-9][0-9]\+h.*//'`
 calendar_summary=`echo "$now_title""$next_summary"`
