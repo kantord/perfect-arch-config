0e55965476216a2fb68bfb350e8196501c86f4f9
Dániel Kántor <git@daniel-kantor.com>
2019-09-09 11:51:49 +0200

    fix wp_init.sh

--- switch_monitor_mode.sh
+++ switch_monitor_mode.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 autorandr --change $1
-~/repos/perfect-arch-config/dotfiles/wpgtk/.config/wpg/wp_init.sh
+~/repos/perfect-arch-config/wp_init.sh
---
+++ wp_init.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+command_to_run=$(cat ~/repos/perfect-arch-config/dotfiles/wpgtk/.config/wpg/wp_init.sh | grep wpg | sed "s/rsL/s/")
+echo $command_to_run
+eval $command_to_run
