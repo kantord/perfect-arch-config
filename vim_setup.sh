dec487f69e0c8569b4807183f931118c0b2c6350
Dániel Kántor <git@daniel-kantor.com>
2019-09-12 21:18:52 +0200

    .

--- vim_setup.sh
+++ vim_setup.sh
@@ -7,12 +7,12 @@ echo "Updating vim plugins"
 vim -E -c PlugUpdate -c qa
 
 echo "Installing coc plugins"
-vim -E -c CocInstall coc-sources -c qa
-vim -E -c CocInstall coc-tsserver -c qa
-vim -E -c CocInstall coc-json -c qa
-vim -E -c CocInstall coc-flow -c qa
-vim -E -c CocInstall coc-yaml -c qa
-vim -E -c CocInstall coc-python -c qa
-vim -E -c CocInstall coc-marketplace -c qa
+vim -E -c "CocInstall coc-sources" -c qa
+vim -E -c "CocInstall coc-tsserver" -c qa
+vim -E -c "CocInstall coc-json" -c qa
+vim -E -c "CocInstall coc-flow" -c qa
+vim -E -c "CocInstall coc-yaml" -c qa
+vim -E -c "CocInstall coc-python" -c qa
+vim -E -c "CocInstall coc-marketplace" -c qa
 
 echo "Done with vim setup"
