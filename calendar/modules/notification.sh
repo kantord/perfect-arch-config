e9c414cd12396567b72a9a408e89293729b2a11f
Dániel Kántor <git@daniel-kantor.com>
2020-09-11 12:53:04 +0200

    move status-one-liner to a git submodule

--- calendar/modules/notification.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cat /tmp/last_notification.txt | sed 's/$/\n/' | sed 's/^/🔔 /' | grep -v '^🔔 $'
--- calendar/modules/pomodoro.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-i3-gnome-pomodoro status | sed 's/^/🍅 /' | grep -v "^🍅 $"
