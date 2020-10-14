05bef973a2756bd8ca09b53d44f349b1243070a0
Dániel Kántor <git@daniel-kantor.com>
2020-10-14 09:59:01 +0200

    set vim_setup to use nvim

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
 echo "Installing vim plugins"
-vim -E -c PlugInstall -c qa
+nvim -E -c PlugInstall -c qa
 
 echo "Updating vim plugins"
-vim -E -c PlugUpdate -c qa
+nvim -E -c PlugUpdate -c qa
 
 echo "Done with vim setup"
 
